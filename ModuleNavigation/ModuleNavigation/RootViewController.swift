a0dc5d5601bfe7f07294166f0f2b69d5d9fd94b0
nickbryanmiller <nickbryanmiller@gmail.com>
2019-02-21 21:30:04 -0800

    update

---
+++ ModuleNavigation/ModuleNavigation/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModuleNavigation
+//
+//  Created by Nicholas Miller on 2/21/19.
+//  Copyright © 2019 nickbryanmiller. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+
+
+}
+
